70d5b8d459482b77dc774da6ef9e7eedb32e3c94
Dicle <diclebahceli24@gmail.com>
2022-04-24 23:39:51 +0300

    create notification page

---
+++ startUpper/app/src/main/java/com/example/startupper/Notification.kt
@@ -0,0 +1,11 @@
+package com.example.startupper
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Notification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
