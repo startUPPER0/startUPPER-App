7166a1bb2cd94a8807f47adcd724ddc10fb3476b
Mustafa Yencilek <gnrlsam@gmail.com>
2022-05-01 17:26:12 +0300

    feed page created and login button activated"

--- startUpper/app/src/main/java/com/example/startupper/Login.kt
+++ startUpper/app/src/main/java/com/example/startupper/Login.kt
@@ -1,11 +1,60 @@
 package com.example.startupper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
+    final lateinit var email: String
+    final lateinit var passw: String
+    lateinit var edittextMail: EditText
+    lateinit var edittextPassw: EditText
+    lateinit var loginButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        email = "startupper@gmail.com"
+        passw = "erdinc0808"
+        edittextMail = findViewById(R.id.email)
+        edittextPassw = findViewById(R.id.password)
+        loginButton = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            logInMethod()
+        }
+
+
+    }
+
+    fun logInMethod() {
+
+        var emailEntered = edittextMail.text.toString()
+        var passwEntered = edittextPassw.text.toString()
+        if (emailEntered.equals(email)) {
+            if (passwEntered.equals(passw)) {
+                Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+
+                val intentFeed = Intent(this, FeedActivity::class.java)
+
+                startActivity(intentFeed)
+            } else if (passwEntered.equals("")) {
+                Toast.makeText(this, "Please Enter Your Password", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, "Wrong Password", Toast.LENGTH_SHORT).show()
+            }
+        } else if (emailEntered.equals("")) {
+            Toast.makeText(this, "Please Enter Your e-mail", Toast.LENGTH_SHORT).show()
+
+        } else {
+            Toast.makeText(this, "User Not Found", Toast.LENGTH_SHORT).show()
+
+        }
+
+
     }
 }
