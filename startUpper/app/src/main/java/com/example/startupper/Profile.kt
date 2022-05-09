b0504032699fcde69d221b2f1f61d810939e2928
Mustafa Yencilek <gnrlsam@gmail.com>
2022-05-09 19:52:21 +0300

    update profile and toolbar

--- startUpper/app/src/main/java/com/example/startupper/Profile.kt
+++ startUpper/app/src/main/java/com/example/startupper/Profile.kt
@@ -2,10 +2,30 @@ package com.example.startupper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class Profile : AppCompatActivity() {
+    private lateinit var appbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        appbar = findViewById(R.id.profiletoolbar)
+        setSupportActionBar(appbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.profile_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+        /*  when (item.itemId) {
+              R.id.editProfile ->
+                  R.id.logoutProfile->
+          }*/
     }
 }
+
