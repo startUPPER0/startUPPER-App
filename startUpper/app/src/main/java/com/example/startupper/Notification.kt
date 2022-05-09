ec70ef748c504bdf26731df246e53cf5945cb870
Dicle <diclebahceli24@gmail.com>
2022-05-09 20:48:36 +0300

    change notfication page's structure

--- startUpper/app/src/main/java/com/example/startupper/Notification.kt
+++ startUpper/app/src/main/java/com/example/startupper/Notification.kt
@@ -2,10 +2,21 @@ package com.example.startupper
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.startupper.adapter.notificationRecylerAdapter
+import com.example.startupper.data.dataSource
 
 class Notification : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val myDataset = dataSource().loadNotificationModel()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = notificationRecylerAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(false)
+
+
     }
 }
--- startUpper/app/src/main/java/com/example/startupper/adapter/notificationRecylerAdapter.kt
+++ startUpper/app/src/main/java/com/example/startupper/adapter/notificationRecylerAdapter.kt
@@ -1,4 +1,38 @@
 package com.example.startupper.adapter
 
-class notificationRecylerAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.startupper.R
+import com.example.startupper.model.notificationModel
+
+class notificationRecylerAdapter(private  val context : Context,
+                                 private val dataset : List<notificationModel>):
+                                RecyclerView.Adapter<notificationRecylerAdapter.ItemViewHolder>(){
+
+    class  ItemViewHolder(private  val view : View): RecyclerView.ViewHolder(view){
+        val textView : TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): notificationRecylerAdapter.ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_notification, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: notificationRecylerAdapter.ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text =  context.resources.getString(item.stringResourceId)
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+
+
 }
